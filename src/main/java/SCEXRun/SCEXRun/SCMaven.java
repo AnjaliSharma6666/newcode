8bf27ea621b0a06811824b805446d23fe8cdb3e5
anjie87 <anjali.sharma87@outlook.com>
2020-08-30 14:26:41 -0500

    Sc execution code from TC

---
+++ src/main/java/SCEXRun/SCEXRun/SCMaven.java
@@ -0,0 +1,71 @@
+package SCEXRun.SCEXRun;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SCMaven {
+
+	public static void main(String[] args) {
+
+		try {
+			SCMaven.SCRunExecution(45, "http://desktop-5ivatg8:19120", "327981e9-d9f0-4e00-9a78-1bcb86078ec2");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void SCRunExecution(int SC_ExecutionNodeId, String SC_Host, String SC_Token) throws Exception {
+
+		try {
+			URL url = new URL(SC_Host + "/Services1.0/execution/executionplanruns?nodeId=" + SC_ExecutionNodeId);
+
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Accept", "application/json;charset=UTF-8");
+			conn.setRequestProperty("SC-SESSION-ID", SC_Token);
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setDoOutput(true);
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP Error code : " + conn.getResponseCode());
+			}
+
+			else {
+				BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+
+				StringBuilder response = new StringBuilder();
+				String responseLine = null;
+
+				while ((responseLine = br.readLine()) != null) {
+					response.append(responseLine.trim());
+				}
+				System.out.println(response.toString());
+				String JsonString = response.toString();
+				Map<String, String> map = new HashMap<String, String>();
+
+				String[] JStr = JsonString.split("}],");
+				String Data = JStr[1];
+				// System.out.println(Data);
+				String[] KeyVal = Data.replaceAll(",", ":").split(":");
+
+				for (int i = 0; i < KeyVal.length; i = i + 2) {
+					map.put(KeyVal[i], KeyVal[i + 1]);
+				}
+
+				System.out.println("Started run with Run id is " + map.get("executionPlanRunId")
+				+ "for execution plan is " + map.get("executionPlanName"));
+				Thread.sleep(20000);
+			}
+			conn.disconnect();
+
+		} catch (Exception e) {
+			System.out.println("Exception:- " + e);
+		}
+	}
+
+}
